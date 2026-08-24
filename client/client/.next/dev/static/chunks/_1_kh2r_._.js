42f59d6c433c4f08af23a6b9a41b1d2682b702db
Ritika <ritikakushwaha62@gmail.com>
2026-08-24 16:27:41 +0530

    chore: remove build artifacts and refine .gitignore

--- client/client/.next/dev/static/chunks/_1_kh2r_._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0382oq5._.js","static/chunks/src_app_globals_162hn9o.css","static/chunks/src_16jj0kt._.js","static/chunks/node_modules_0f-lsea._.js"],
-    source: "entry"
-});
